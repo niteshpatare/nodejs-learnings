366c84d76ca51e0eefbee382c722c76a1474bda7
Nitesh <niteshp27@gmail.com>
2013-04-18 00:08:05 +0530

    routing

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var http = require("http");
+var url = require("url");
 
-function start()
+function start(route)
 {
   function onRequest(request, response) {
     console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " received.");
+	route(pathname);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Hello World</h1>");
     response.end();
   }
   http.createServer(onRequest).listen(8888);
 }
 exports.start=start;
-
-
 
 console.log("Server has started.");
