c9596a5545d0cc889083e09ff6d8fdc287a8289a
Nitesh <niteshp27@gmail.com>
2013-04-20 02:55:13 +0530

    deploy on azure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var url = require("url");
-
+var port = process.env.PORT || 8888;
 function start(route)
 {
   function onRequest(request, response) {
@@ -12,7 +12,7 @@ function start(route)
     response.write("<h1>Hello World</h1>");
     response.end();
   }
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(port);
 }
 exports.start=start;
 
